92b04c7e3a0569297fe628c163217e51b25b7db1
GameDog9988 <alex@grattan.me>
2022-11-07 20:09:15 -0500

    feat: create user model

---
+++ lib/models/user.dart
@@ -0,0 +1,31 @@
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+  final String image;
+  final bool notificationsEnabled;
+  final int notificationDayAmount;
+
+  User({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.image,
+    this.notificationsEnabled = true,
+    this.notificationDayAmount = 3,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      firstName: json["firstName"],
+      lastName: json["lastName"],
+      email: json["email"],
+      password: json["password"],
+      image: json["image"],
+      notificationsEnabled: json["notificationsEnabled"],
+      notificationDayAmount: json["notificationDayAmount"],
+    );
+  }
+}
