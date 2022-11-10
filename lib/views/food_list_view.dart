afd882944e66cb01c8a20f9721c2adaf4ec35b84
jennzheng12315 <jennzheng12315@gmail.com>
2022-11-10 01:55:43 -0500

    feat: added dropdown placeholder

--- lib/views/food_list_view.dart
+++ lib/views/food_list_view.dart
@@ -5,6 +5,13 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
+const List<String> inventories = <String>[
+  'All Food Items',
+  'Fridge',
+  'Freezer',
+  'Pantry'
+];
+
 class ItemListView extends StatefulWidget {
   const ItemListView({super.key});
 
@@ -51,48 +58,69 @@ class _ItemListViewState extends State<ItemListView> {
           backgroundColor: Colors.yellow[100],
           onTap: _onItemTapped,
         ),
-        body: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: foodItems.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                  margin: const EdgeInsets.all(16),
-                  padding: const EdgeInsets.all(16),
-                  decoration: const BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Text(
-                          "${foodItems[index].quantity.toString()}x ${foodItems[index].name}",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        margin: const EdgeInsets.only(right: 4),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Text(FoodListEntryViewModel.getInitials(
-                            foodItems[index])),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(20))),
-                        child: Text(FoodListEntryViewModel.expirationString(
-                            foodItems[index])),
-                      )
-                    ],
-                  ));
-            }));
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              padding: const EdgeInsets.only(top: 28, left: 24, right: 24),
+              // color: Colors.white,
+              width: MediaQuery.of(context).size.width,
+              child: Row(children: [
+                const Text(
+                  "inventories dropdown here",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 24),
+                )
+              ]),
+            ),
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: foodItems.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          margin: const EdgeInsets.all(16),
+                          padding: const EdgeInsets.all(16),
+                          decoration: const BoxDecoration(
+                              color: Colors.white70,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  "${foodItems[index].quantity.toString()}x ${foodItems[index].name}",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                margin: const EdgeInsets.only(right: 4),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Text(FoodListEntryViewModel.getInitials(
+                                    foodItems[index])),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(20))),
+                                child: Text(
+                                    FoodListEntryViewModel.expirationString(
+                                        foodItems[index])),
+                              )
+                            ],
+                          ));
+                    }))
+          ],
+        ));
   }
 }
