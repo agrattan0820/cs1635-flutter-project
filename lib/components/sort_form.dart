10cd09dc507157f436e00eddafe7f2303bb2f076
GameDog9988 <alex@grattan.me>
2022-11-16 19:35:03 -0500

    feat: add save button to sort form

--- lib/components/sort_form.dart
+++ lib/components/sort_form.dart
@@ -59,9 +59,10 @@ class _SortFormState extends State<SortForm> {
                   color: Colors.black,
                 ),
                 selected: defaultChoiceIndex == index,
-                selectedColor: Colors.yellow[800],
+                selectedColor: Colors.amber,
                 backgroundColor: Colors.yellow[100],
                 shape: const StadiumBorder(side: BorderSide()),
+                elevation: 2,
                 onSelected: (value) {
                   setState(() {
                     defaultChoiceIndex = value ? index : defaultChoiceIndex;
@@ -70,79 +71,26 @@ class _SortFormState extends State<SortForm> {
                 },
               );
             }),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                  fixedSize:
+                      MaterialStateProperty.all<Size>(const Size(150, 24))),
+              child: const Text("Save"),
+            ),
           )
         ],
       ),
     );
   }
 }
-
-// class SortForm extends StatelessWidget {
-//   final List<String> options;
-//   final Function(bool, int) onOptionSelect;
-//   final int selectedOption;
-
-//   const SortForm({
-//     super.key,
-//     required this.selectedOption,
-//     required this.options,
-//     required this.onOptionSelect,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           Center(
-//             child: Container(
-//               width: 48,
-//               height: 4,
-//               decoration: BoxDecoration(
-//                 color: Colors.black,
-//                 borderRadius: BorderRadius.circular(16),
-//               ),
-//             ),
-//           ),
-//           const SizedBox(
-//             height: 48,
-//           ),
-//           const Text(
-//             "Sort Items",
-//             style: TextStyle(
-//               fontSize: 32,
-//               fontWeight: FontWeight.bold,
-//             ),
-//             textAlign: TextAlign.left,
-//           ),
-//           const SizedBox(
-//             height: 8,
-//           ),
-//           Wrap(
-//             spacing: 8,
-//             children: List.generate(options.length, (index) {
-//               return ChoiceChip(
-//                 label: Text(
-//                   options[index],
-//                 ),
-//                 labelStyle: const TextStyle(
-//                   color: Colors.black,
-//                 ),
-//                 selected: selectedOption == index,
-//                 selectedColor: Colors.yellow[800],
-//                 backgroundColor: Colors.yellow[100],
-//                 shape: const StadiumBorder(side: BorderSide()),
-//                 onSelected: (value) {
-//                   debugPrint("selected");
-//                   onOptionSelect(value, index);
-//                 },
-//               );
-//             }),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
