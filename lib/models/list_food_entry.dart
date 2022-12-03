541ad0b268c1d2a14dbcfdf6985114631bdc6485
GameDog9988 <alex@grattan.me>
2022-12-02 21:37:32 -0500

    feat: sharing functionality for home list view

--- lib/models/list_food_entry.dart
+++ lib/models/list_food_entry.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:grosseries/view_models/food_item_view_model.dart';
+
+import 'food_item.dart';
+
 class ListFoodEntry {
   final int entryId;
   final int foodId;
@@ -24,5 +29,29 @@ class ListFoodEntry {
       owner: json["owner"],
       dateAdded: json["dateAdded"],
     );
+  }
+
+  @override
+  String toString() {
+    FoodItem? foodItem = FoodItemViewModel.getFoodItem(foodId);
+    Duration timePassed = DateTime.now().difference(dateAdded);
+    Map expirationInfo = {"text": "", "color": Colors.lightGreen};
+
+    if (timePassed.inDays == foodItem!.daysToExpire) {
+      expirationInfo["text"] = "Expires Today";
+      expirationInfo["color"] = Colors.red[300];
+    } else if (timePassed.inDays > foodItem.daysToExpire) {
+      expirationInfo["text"] =
+          "Expired by ${timePassed.inDays - foodItem.daysToExpire} ${(timePassed.inDays - foodItem.daysToExpire > 1) ? "days" : "day"}";
+      expirationInfo["color"] = Colors.red[300];
+    } else {
+      expirationInfo["text"] =
+          "${foodItem.daysToExpire - timePassed.inDays} ${(foodItem.daysToExpire - timePassed.inDays > 1) ? "days" : "day"} left";
+
+      if (foodItem.daysToExpire - timePassed.inDays <= 3) {
+        expirationInfo["color"] = Colors.amber;
+      }
+    }
+    return "${foodItem.name} - ${expirationInfo['text']}\n$storage - #$quantity - $owner";
   }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,8 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   awesome_notifications
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
